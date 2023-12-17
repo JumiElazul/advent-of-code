508f18f147c8b1048022da8a78ef2fb837d68be6
Gregory Coomber <gr.coomber@gmail.com>
2023-12-16 23:01:08 -0800

    Added gitignore to ignore binaries

--- 2023/compile_day.sh
+++ 2023/compile_day.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 clear
-g++ -std=c++2a -g -ggdb -Wall -Wextra -Wconversion -Wsign-conversion -pedantic-errors -fsanitize=address,undefined Day$1_main.cpp -o Day$1
+g++ -std=c++2a -g -ggdb -Wall -Wextra -Wconversion -Wsign-conversion -pedantic-errors -fsanitize=address,undefined Day$1_main.cpp -o Day$1.out
