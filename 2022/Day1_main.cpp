df482c43cc4d8a89a2d2c5b0f32115beb6e2451a
Gregory Coomber <gr.coomber@gmail.com>
2024-01-02 14:25:26 -0800

    Changed day1 solution 2022 to use a heap

--- 2022/Day1_main.cpp
+++ 2022/Day1_main.cpp
@@ -1,66 +1,52 @@
 #include "jumi_utils.h"
 #include <algorithm>
-#include <array>
-#include <iterator>
+#include <queue>
 #include <vector>
-#include <unordered_map>
 
 constexpr const char* filepath = "Day1_input.txt";
 
-struct Elf
+std::vector<int> get_top_nth_elements(const std::priority_queue<int>& prio_queue, size_t nth)
 {
-    std::int32_t elf_number = 0;
-    std::int64_t calories = 0;
-};
+    std::priority_queue<int> temp_heap = prio_queue;
+    std::vector<int> result;
+    result.reserve(nth);
+
+    for (size_t i = 0; i < nth; ++i)
+    {
+        result.emplace_back(temp_heap.top());
+        temp_heap.pop();
+    }
+
+    return result;
+}
 
 int main()
 {
     std::fstream file = jumi::open_file(filepath);
-    std::vector<std::string> lines = jumi::read_lines(file);
-    std::unordered_map<std::int32_t, std::int64_t> elves;
+    std::priority_queue<int, std::vector<int>, std::less<int>> max_heap;
+    std::string line;
 
-    std::int64_t sum_total = 0;
-    std::int32_t elf_number = 1;
-    for (size_t i = 0; i < lines.size(); ++i)
+    int sum = 0;
+    while (std::getline(file, line))
     {
-        if (lines[i].empty())
+        if (line.empty())
         {
-            elves[elf_number++] = sum_total;
-            sum_total = 0;
-            continue;
+            max_heap.push(sum);
+            sum = 0;
         }
-        sum_total += std::stoll(lines[i]);
+        else
+        {
+            sum += std::stoi(line);
+        }
     }
-    elves[elf_number] = sum_total;
 
-    auto it = std::max_element(elves.begin(), elves.end(),
-            [](const auto& a, const auto& b) {
-                return a.second < b.second; 
-            });
-
-    std::cout << "Max element is: " << it->first << " with value: " << it->second << '\n';
-
-    std::vector<std::int64_t> elements;
-    elements.reserve(elves.size());
-    for (std::unordered_map<std::int32_t, std::int64_t>::const_iterator it = elves.begin(); it != elves.end(); ++it)
+    int total_sum = 0;
+    std::vector<int> top3 = get_top_nth_elements(max_heap, 3);
+    for (const auto& num : top3)
     {
-        elements.push_back(it->second);
+        total_sum += num;
     }
-
-    std::nth_element(elements.begin(), elements.begin() + 3, elements.end(), std::greater<std::int64_t>());
-    std::sort(elements.begin(), elements.begin() + 3, std::greater<std::int64_t>());
-
-    if (elements.size() > 3)
-    {
-        elements.resize(3);
-    }
-
-    std::int64_t sum = 0;
-    for (const auto& num : elements)
-    {
-        sum += num;
-    }
-    std::cout << "Max 3 elements sum: " << sum << '\n';
+    std::cout << "Sum of top 3 elements: " << total_sum << '\n';
 
     return 0;
 }
