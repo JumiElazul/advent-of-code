d006dcf3d3b423b539288ad45dfe23786a8ae8b3
Gregory Coomber <gr.coomber@gmail.com>
2024-12-07 21:09:19 -0800

    Finished day 7

--- 2024/c++/day7_main.cpp
+++ 2024/c++/day7_main.cpp
@@ -1,5 +1,7 @@
 #include "jumi_utils.h"
+#include <algorithm>
 #include <iostream>
+#include <numeric>
 #include <string>
 #include <vector>
 
@@ -9,6 +11,16 @@ struct equation
 {
     size_t target;
     std::vector<size_t> operands;
+};
+
+using solvable = bool;
+static std::vector<std::pair<equation, solvable>> s_part_one_solved;
+static std::vector<std::pair<equation, solvable>> s_part_two_solved;
+
+enum class problem_part
+{
+    one,
+    two
 };
 
 std::ostream& operator<<(std::ostream& os, const equation& eq)
@@ -21,6 +33,72 @@ std::ostream& operator<<(std::ostream& os, const equation& eq)
     return os;
 }
 
+size_t concat_value(size_t op1, size_t op2)
+{
+    std::string o1 = std::to_string(op1);
+    std::string o2 = std::to_string(op2);
+    std::string result = o1 + o2;
+    return std::stoull(result);
+}
+
+void backtrack(const equation& eq, size_t index, size_t current_val, std::vector<char>& ops,
+        std::vector<std::vector<char>>& solutions, problem_part part)
+{
+    if (index == eq.operands.size() - 1)
+    {
+        if (current_val == eq.target)
+        {
+            solutions.push_back(ops);
+        }
+        return;
+    }
+    size_t next_operand = eq.operands[index + 1];
+
+    if (part == problem_part::one)
+    {
+        ops.push_back('+');
+        backtrack(eq, index + 1, current_val + next_operand, ops, solutions, problem_part::one);
+        ops.pop_back();
+
+        ops.push_back('*');
+        backtrack(eq, index + 1, current_val * next_operand, ops, solutions, problem_part::one);
+        ops.pop_back();
+    }
+    else
+    {
+        ops.push_back('+');
+        backtrack(eq, index + 1, current_val + next_operand, ops, solutions, problem_part::two);
+        ops.pop_back();
+
+        ops.push_back('*');
+        backtrack(eq, index + 1, current_val * next_operand, ops, solutions, problem_part::two);
+        ops.pop_back();
+
+        ops.push_back('|');
+        size_t concat_val = concat_value(current_val, next_operand);
+        backtrack(eq, index + 1, concat_val, ops, solutions, problem_part::two);
+        ops.pop_back();
+    }
+}
+
+bool solve_equation(const equation& eq, problem_part part)
+{
+    if (eq.operands.empty())
+        return false;
+
+    std::vector<char> ops;
+    std::vector<std::vector<char>> solutions;
+
+    size_t start_val = eq.operands[0];
+    backtrack(eq, 0, start_val, ops, solutions, part);
+
+    if (solutions.empty())
+    {
+        return false;
+    }
+    return true;
+}
+
 std::vector<equation> get_operations_from_input(const std::vector<std::string>& contents)
 {
     std::vector<equation> eqs;
@@ -29,33 +107,77 @@ std::vector<equation> get_operations_from_input(const std::vector<std::string>& 
         equation new_eq;
         size_t index = line.find(':');
         std::string slice = line.substr(0, index);
-        new_eq.target = std::stoul(slice);
+        new_eq.target = std::stoull(slice);
 
-        index = line.find_first_of(' ');
-        std::string chopped_str = line.substr(index + 1);
+        std::string chopped_str = line.substr(index + 2);
         std::vector<std::string> split = jumi::split(chopped_str, ' ');
         for (const std::string& num : split)
         {
-            new_eq.operands.push_back(std::stoul(num));
+            new_eq.operands.push_back(std::stoull(num));
         }
         eqs.push_back(new_eq);
     }
     return eqs;
 }
 
-void part_one(const std::vector<std::string>& contents)
+void part_one(const std::vector<equation>& equations)
 {
-    std::vector<equation> equations = get_operations_from_input(contents);
+    std::vector<equation> solved;
 
     for (const auto& eq : equations)
     {
-
+        if (solve_equation(eq, problem_part::one))
+        {
+            solved.push_back(eq);
+            s_part_one_solved.push_back({ eq, true });
+        }
+        else
+        {
+            s_part_one_solved.push_back({ eq, false });
+        }
     }
+
+    size_t result = 0;
+    for (const equation& eq : solved)
+    {
+        result += eq.target;
+    }
+
+    std::cout << "[Part One] The final result is " << result << '\n';
+}
+
+void part_two(const std::vector<equation>& equations)
+{
+    std::vector<equation> solved;
+
+    for (const auto& eq : equations)
+    {
+        if (solve_equation(eq, problem_part::two))
+        {
+            solved.push_back(eq);
+            s_part_two_solved.push_back({ eq, true });
+        }
+        else
+        {
+            s_part_two_solved.push_back({ eq, false });
+        }
+    }
+
+    size_t result = 0;
+    for (const equation& eq : solved)
+    {
+        result += eq.target;
+    }
+
+    std::cout << "[Part Two] The final result is " << result << '\n';
 }
 
 int main()
 {
     std::fstream file(filepath);
     std::vector<std::string> contents = jumi::read_lines(file);
-    part_one(contents);
+    std::vector<equation> equations = get_operations_from_input(contents);
+    part_one(equations);
+    part_two(equations);
+    // 169122112716571
 }
