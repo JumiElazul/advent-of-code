c1bf3a871c6b86f8b1d09de9a1dafb7fb83760c7
Gregory Coomber <gr.coomber@gmail.com>
2024-12-10 23:45:46 -0800

    Day 11 part 1 complete

--- 2024/c++/day11_main.cpp
+++ 2024/c++/day11_main.cpp
@@ -1,12 +1,11 @@
 #include "jumi_utils.h"
 #include <iostream>
 #include <vector>
-#include <deque>
 #include <string>
 
-std::deque<size_t> get_stones(const std::string& line)
+std::vector<size_t> get_stones(const std::string& line)
 {
-    std::deque<size_t> stones;
+    std::vector<size_t> stones;
     std::vector<std::string> split = jumi::split(line, ' ');
     for (const std::string& str : split)
     {
@@ -15,23 +14,73 @@ std::deque<size_t> get_stones(const std::string& line)
     return stones;
 }
 
-void part_one(std::deque<size_t>& stones)
+int count_digits(size_t num)
 {
-    for (int i = 0; i < 25; ++i)
+    int count = 0;
+    do
     {
-        for (auto it = stones.begin(); it != stones.end(); ++it)
-        {
+        num /= 10;
+        ++count;
+    } while (num != 0);
+    return count;
+}
 
+void calculate_part(std::vector<size_t>& stones, int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        std::vector<size_t> next_stones;
+        next_stones.reserve(stones.size() * 2);
+
+        for (auto& stone : stones)
+        {
+            if (stone == 0)
+            {
+                next_stones.push_back(1);
+            }
+            else
+            {
+                int num_digits = count_digits(stone);
+                if (num_digits % 2 == 0)
+                {
+                    int half = num_digits / 2;
+                    size_t divisor = 1;
+
+                    for (int i = 0; i < half; ++i)
+                        divisor *= 10;
+
+                    size_t left = stone / divisor;
+                    size_t right = stone % divisor;
+                    next_stones.push_back(left);
+                    next_stones.push_back(right);
+                }
+                else
+                {
+                    stone *= 2024;
+                    next_stones.push_back(stone);
+                }
+            }
         }
+        stones.swap(next_stones);
     }
 
-    std::cout << "[Part One] The final number of stones is " << stones.size() << '\n';
+    std::cout << "[" << n << " iterations] The final number of stones is " << stones.size() << '\n';
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& list)
+{
+    for (const auto& elem : list)
+        os << elem << '\n';
+
+    return os;
 }
 
 int main()
 {
     std::fstream file = jumi::open_file("input/day11_input.txt");
     std::string line = jumi::stringify_file(file);
-    std::deque<size_t> stones = get_stones(line);
-    part_one(stones);
+    std::vector<size_t> stones = get_stones(line);
+    calculate_part(stones, 25);
+    calculate_part(stones, 75);
 }
