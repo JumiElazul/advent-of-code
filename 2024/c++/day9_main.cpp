c1c59ed173acbd4d14758af2c9e747febe34810d
Gregory Coomber <gr.coomber@gmail.com>
2024-12-11 14:49:04 -0800

    Finished day 9 part 1

--- 2024/c++/day9_main.cpp
+++ 2024/c++/day9_main.cpp
@@ -1,70 +1,86 @@
 #include "jumi_utils.h"
 #include <algorithm>
-#include <iostream>
 #include <iterator>
 #include <string>
-#include <sstream>
 #include <vector>
 
 constexpr const char* filepath = "input/day9_input.txt";
 
-enum class layout
-{
-    file,
-    freespace
-};
-
 struct data_block
 {
-    std::vector<int> ids;
-    int capacity;
+    int id;
 };
 
-class disk
+std::vector<data_block> get_data_block_representation(const std::string& diskmap)
 {
-public:
-    disk(const std::string& input) : _data_blocks()
+    std::vector<data_block> blocks;
+    blocks.reserve(diskmap.size());
+    for (int i = 0; i < diskmap.size(); ++i)
     {
-        int block_id = 0;
-        int i = 0;
-        _data_blocks.resize(input.size());
-
-        for (const char& c : input)
+        int count = diskmap[i] - '0';
+        while (count > 0)
         {
-            const int block_size = c - '0';
-            _data_blocks[i].capacity = block_size;
-
             if (i % 2 == 0)
             {
-                _data_blocks[i].ids = std::vector<int>(block_size, block_id);
-                ++block_id;
+                blocks.emplace_back(i / 2);
             }
-            ++i;
+            else
+            {
+                blocks.emplace_back(-1);
+            }
+            --count;
         }
     }
 
-    size_t compute_checksum()
-    {
+    return blocks;
+}
 
+size_t compute_checksum(const std::vector<data_block>& data_blocks)
+{
+    size_t checksum = 0;
+
+    for (int i = 0; i < data_blocks.size(); ++i)
+    {
+        if (data_blocks[i].id != -1)
+        {
+            checksum += (i * data_blocks[i].id);
+        }
     }
 
-private:
-    std::vector<data_block> _data_blocks;
-};
+    return checksum;
+}
 
-void part_one(std::string& diskmap_representation)
+void part_one(std::vector<data_block>& data_blocks)
 {
-    std::cout << diskmap_representation << '\n';
+    auto find_empty = [](const data_block& block) -> bool {
+        return block.id == -1;
+    };
+
+    auto find_file = [](const data_block& block) {
+        return block.id >= 0;
+    };
+
+    std::vector<data_block>::iterator empty = std::find_if(data_blocks.begin(), data_blocks.end(), find_empty);
+    std::vector<data_block>::reverse_iterator file = std::find_if(data_blocks.rbegin(), data_blocks.rend(), find_file);
+
+    while (empty != data_blocks.end() && file != data_blocks.rend() && empty < std::prev(file.base())) 
+    {
+        *empty = *file;
+        file->id = -1;
+
+        empty = std::find_if(empty, data_blocks.end(), find_empty);
+        file = std::find_if(file, data_blocks.rend(), find_file);
+    }
+
+    size_t checksum = compute_checksum(data_blocks);
+    std::cout << "[Part One] The checksum is " << checksum << '\n';
 }
 
 int main()
 {
     std::fstream file = jumi::open_file(filepath);
     std::string contents = jumi::stringify_file(file);
-    std::string test_str = "35493538261755937276396055768953609918499";
-    disk d(test_str);
-    size_t result = d.compute_checksum();
-    std::cout << "test_str: " << test_str << '\n';
-    std::cout << "[Part One] The final result checksum is " << result << '\n';
+    std::vector<data_block> data_blocks = get_data_block_representation(contents);
+    part_one(data_blocks);
 }
 
