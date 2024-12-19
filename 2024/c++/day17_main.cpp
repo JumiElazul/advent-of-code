47f0953e5c0453e4eec35602a948fbb1a1c7536d
Gregory Coomber <gr.coomber@gmail.com>
2024-12-19 14:14:32 -0800

    Finished day 5 top sort part two

--- 2024/c++/day17_main.cpp
+++ 2024/c++/day17_main.cpp
@@ -1,4 +1,6 @@
 #include "jumi_utils.h"
+#include <csignal>
+#include <fstream>
 #include <iostream>
 #include <vector>
 #include <string>
@@ -6,36 +8,182 @@
 
 struct cpu_register
 {
-    int a;
-    int b;
-    int c;
+    long long a;
+    long long b;
+    long long c;
 };
 
 class cpu
 {
+using opcode = long long;
+using operand = long long;
 public:
-    cpu(const std::vector<unsigned char>& instructions)
-        : _registers{ 0, 0, 0 }
-        , _instructions{ std::move(instructions) }
+    cpu(const cpu_register& registers, const std::vector<long long>& instructions)
+        : _registers(registers)
+        , _instructions(instructions)
         , _ip(0)
+        , _halt(false)
+        , _output()
     {
 
     }
 
-    void increment_ip(int value = 2)
+    [[nodiscard]] bool halted() const noexcept { return _halt; }
+    [[nodiscard]] const std::vector<long long>& get_output() const noexcept { return _output; }
+
+    void run()
     {
-        _ip += value;
+        while (!halted())
+        {
+            execute_instruction();
+        }
+    }
+
+    void execute_instruction()
+    {
+        if (_ip >= _instructions.size())
+        {
+            _halt = true;
+            return;
+        }
+
+        bool dont_jump = false;
+        opcode opcode_ = _instructions[_ip];
+        operand operand_ = _instructions[_ip + 1];
+        operand combo_operand_ = get_combo_operand(operand_);
+
+        // Combo operands 0 through 3 represent literal values 0 through 3.
+        // Combo operand 4 represents the value of register A.
+        // Combo operand 5 represents the value of register B.
+        // Combo operand 6 represents the value of register C.
+        // Combo operand 7 is reserved and will not appear in valid programs.
+        switch (opcode_)
+        {
+            // The adv instruction (opcode 0) performs division. The numerator is the value in the A register. The denominator
+            // is found by raising 2 to the power of the instruction's combo operand. (So, an operand of 2 would divide A by 4
+            // (2^2); an operand of 5 would divide A by 2^B.) The result of the division operation is truncated to an long longeger 
+            // and then written to the A register.
+            case 0:
+            {
+                _registers.a = _registers.a >> combo_operand_;
+            } break;
+            // The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand,
+            // then stores the result in register B.
+            case 1:
+            {
+                _registers.b = _registers.b ^ operand_;
+            } break;
+            // The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest
+            // 3 bits), then writes that value to the B register.
+            case 2:
+            {
+                _registers.b = combo_operand_ % 8;
+            } break;
+            // The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero, it jumps
+            // by setting the instruction polong longer to the value of its literal operand; if this instruction jumps, the instruction
+            // polong longer is not increased by 2 after this instruction.
+            case 3:
+            {
+                if (_registers.a != 0)
+                {
+                    _ip = operand_;
+                    dont_jump = true;
+                }
+            } break;
+            // The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result
+            // in register B. (For legacy reasons, this instruction reads an operand but ignores it.)
+            case 4:
+            {
+                _registers.b = _registers.b ^ _registers.c;
+            } break;
+            // The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value. (If
+            // a program outputs multiple values, they are separated by commas.)
+            case 5:
+            {
+                _output.push_back(combo_operand_ % 8);
+            } break;
+            // The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B
+            // register. (The numerator is still read from the A register.)
+            case 6:
+            {
+                _registers.b = _registers.a >> combo_operand_;
+            } break;
+            // The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C
+            // register. (The numerator is still read from the A register.)
+            case 7:
+            {
+                _registers.c = _registers.a >> combo_operand_;
+            } break;
+        }
+
+        if (dont_jump)
+            return;
+
+        increment_ip(2);
     }
 
 private:
     cpu_register _registers;
-    std::vector<unsigned char> _instructions;
-    int _ip;
+    std::vector<long long> _instructions;
+    long long _ip;
+    bool _halt;
+    std::vector<long long> _output;
+
+    void increment_ip(long long value)
+    {
+        _ip += value;
+    }
+
+    long long get_combo_operand(operand op)
+    {
+        if (op <= 3)
+            return op;
+
+        if (op == 4)
+            return _registers.a;
+        else if (op == 5)
+            return _registers.b;
+        else if (op == 6)
+            return _registers.c;
+        else
+            throw std::runtime_error("Invalid operand");
+    }
 };
 
-std::vector<unsigned char> get_instructions(const std::vector<std::string>& contents)
+cpu_register get_init_registers(const std::vector<std::string>& contents)
 {
-    std::vector<unsigned char> instructions;
+    std::string target_a = "Register A: ";
+    std::string target_b = "Register B: ";
+    std::string target_c = "Register C: ";
+    std::string cut_str = ": ";
+
+    cpu_register initial{};
+
+    for (const std::string& line : contents)
+    {
+        if (line.find(target_a) != std::string::npos)
+        {
+            std::string cut = line.substr(line.find(cut_str) + 2);
+            initial.a = std::stoi(cut);
+        }
+        else if (line.find(target_b) != std::string::npos)
+        {
+            std::string cut = line.substr(line.find(cut_str) + 2);
+            initial.b = std::stoi(cut);
+        }
+        else if (line.find(target_c) != std::string::npos)
+        {
+            std::string cut = line.substr(line.find(cut_str) + 2);
+            initial.c = std::stoi(cut);
+        }
+    }
+
+    return initial;
+}
+
+std::vector<long long> get_instructions(const std::vector<std::string>& contents)
+{
+    std::vector<long long> instructions;
     std::string target = "Program: ";
 
     for (const std::string& line : contents)
@@ -47,7 +195,7 @@ std::vector<unsigned char> get_instructions(const std::vector<std::string>& cont
             std::string token;
             while (std::getline(ss, token, ','))
             {
-                instructions.push_back(std::stoi(token));
+                instructions.push_back(std::stoll(token));
             }
         }
     }
@@ -55,9 +203,52 @@ std::vector<unsigned char> get_instructions(const std::vector<std::string>& cont
     return instructions;
 }
 
+void part_one(cpu& cpu_)
+{
+    cpu_.run();
+    const std::vector<long long>& output = cpu_.get_output();
+    size_t size = output.size();
+    for (size_t i = 0; i < size; ++i)
+    {
+        std::cout << output[i];
+        if (i != size - 1)
+            std::cout << ',';
+    }
+
+    std::cout << '\n';
+}
+
+void part_two(const std::vector<std::string>& contents, const std::vector<long long>& instructions)
+{
+    // cpu_register initial_registers = get_init_registers(contents);
+    // initial_registers.a = 0;
+
+    // for (long long i = 0; i < 8; ++i)
+    // {
+    //     cpu cpu_p2(initial_registers, instructions);
+    //     cpu_p2.run();
+
+    //     const std::vector<long long>& output = cpu_p2.get_output();
+    //     if (output == instructions)
+    //     {
+    //         std::cout << "[Part Two] " << i << '\n';
+    //     }
+    // }
+}
+
 int main()
 {
     std::fstream file = jumi::open_file("input/day17_input.txt");
     std::vector<std::string> contents = jumi::read_lines(file);
-    std::vector<unsigned char> instructions = get_instructions(contents);
+    std::vector<long long> instructions = get_instructions(contents);
+
+    // for (const auto& i : instructions)
+    // {
+    //     std::cout << i << ',';
+    // }
+
+    cpu_register initial_registers = get_init_registers(contents);
+    cpu cpu_p1(initial_registers, instructions);
+    part_one(cpu_p1);
+    part_two(contents, instructions);
 }
--- 2024/c++/day5_main.cpp
+++ 2024/c++/day5_main.cpp
@@ -1,14 +1,16 @@
 #include "jumi_utils.h"
 #include <iostream>
 #include <algorithm>
+#include <stdexcept>
 #include <unordered_map>
 #include <string>
+#include <variant>
 #include <vector>
 #include <unordered_set>
 
-std::unordered_map<int, std::vector<int>> build_print_map(const std::vector<std::string>& contents)
+std::unordered_map<int, std::vector<int>> build_adj_list(const std::vector<std::string>& contents)
 {
-    std::unordered_map<int, std::vector<int>> print_map;
+    std::unordered_map<int, std::vector<int>> adj_list;
     for (const std::string& line : contents)
     {
         auto it = line.find('|');
@@ -18,11 +20,11 @@ std::unordered_map<int, std::vector<int>> build_print_map(const std::vector<std:
             std::string second = line.substr(it + 1);
             int f = std::stoi(first);
             int s = std::stoi(second);
-            print_map[f].push_back(s);
+            adj_list[f].push_back(s);
         }
     }
 
-    return print_map;
+    return adj_list;
 }
 
 std::vector<std::vector<int>> build_instruction_map(const std::vector<std::string>& contents)
@@ -53,14 +55,14 @@ std::vector<std::vector<int>> build_instruction_map(const std::vector<std::strin
     return instruction_map;
 }
 
-bool is_valid_instruction(const std::unordered_map<int, std::vector<int>>& print_map, const std::vector<int>& instruction_list)
+bool is_valid_instruction(const std::unordered_map<int, std::vector<int>>& adj_list, const std::vector<int>& instruction_list)
 {
     std::vector<int> printed;
     bool valid = true;
 
     for (const int i : instruction_list)
     {
-        const std::vector<int>& rules = print_map.at(i);
+        const std::vector<int>& rules = adj_list.at(i);
         for (const int rule : rules)
         {
             if (std::find(printed.begin(), printed.end(), rule) != printed.end())
@@ -86,12 +88,12 @@ std::ostream& operator<<(std::ostream& os, const std::vector<int>& list)
     return os;
 }
 
-void part_one(const std::unordered_map<int, std::vector<int>>& print_map, const std::vector<std::vector<int>>& instruction_map)
+void part_one(const std::unordered_map<int, std::vector<int>>& adj_list, const std::vector<std::vector<int>>& instruction_map)
 {
     size_t sum = 0;
     for (const std::vector<int>& instruction_list : instruction_map)
     {
-        bool valid = is_valid_instruction(print_map, instruction_list);
+        bool valid = is_valid_instruction(adj_list, instruction_list);
         if (valid)
         {
             sum += instruction_list.at(instruction_list.size() / 2);
@@ -101,22 +103,64 @@ void part_one(const std::unordered_map<int, std::vector<int>>& print_map, const 
     std::cout << "[Part One] The final result is " << sum << '\n';
 }
 
-void part_two(const std::unordered_map<int, std::vector<int>>& print_map, const std::vector<std::vector<int>>& instruction_map)
+void part_two(const std::unordered_map<int, std::vector<int>>& adj_list, const std::vector<std::vector<int>>& instruction_map)
 {
-    size_t sum = 0;
+    std::vector<std::vector<int>> fixed_instructions;
+    fixed_instructions.reserve(128);
+
     for (const std::vector<int>& instruction_list : instruction_map)
     {
-        bool valid = is_valid_instruction(print_map, instruction_list);
+        bool valid = is_valid_instruction(adj_list, instruction_list);
         if (!valid)
         {
-            std::cout << "Invalid instruction list: " << instruction_list << '\n';
+            std::vector<int> top_sort;
             std::unordered_set<int> visited;
+            std::unordered_set<int> recursion_stack;
+            top_sort.reserve(32);
+
+            auto dfs = [&](auto& self, int node) -> void {
+                if (recursion_stack.find(node) != visited.end())
+                    throw std::runtime_error("Graph contains a cycle");
+
+                if (visited.find(node) != visited.end())
+                    return;
+
+                recursion_stack.emplace(node);
+                visited.emplace(node);
+
+                auto it = adj_list.find(node);
+                if (it != adj_list.end())
+                {
+                    for (const int neighbour : it->second)
+                    {
+                        if (std::find(instruction_list.begin(), instruction_list.end(), neighbour) != instruction_list.end())
+                        {
+                            self(self, neighbour);
+                        }
+                    }
+                }
+
+                recursion_stack.erase(node);
+                top_sort.push_back(node);
+            };
 
             for (const int i : instruction_list)
             {
-
+                dfs(dfs, i);
             }
+
+            std::reverse(top_sort.begin(), top_sort.end());
+            fixed_instructions.push_back(top_sort);
         }
+
+    }
+
+    size_t sum = 0;
+    for (const std::vector<int>& v : fixed_instructions)
+    {
+        const size_t& s = v.size();
+        if (!v.empty())
+            sum += v[s / 2];
     }
 
     std::cout << "[Part Two] The final result is " << sum << '\n';
@@ -126,9 +170,9 @@ int main()
 {
     std::fstream file = jumi::open_file("input/day5_input.txt");
     std::vector<std::string> contents = jumi::read_lines(file);
-    std::unordered_map<int, std::vector<int>> print_map = build_print_map(contents);
+    std::unordered_map<int, std::vector<int>> adj_list = build_adj_list(contents);
     std::vector<std::vector<int>> instruction_map = build_instruction_map(contents);
 
-    part_one(print_map, instruction_map);
-    part_two(print_map, instruction_map);
+    part_one(adj_list, instruction_map);
+    part_two(adj_list, instruction_map);
 }
