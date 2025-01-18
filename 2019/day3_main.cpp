052f1d5f30e83d06437aa02c40376121484c30da
Gregory Coomber <gr.coomber@gmail.com>
2025-01-17 16:33:51 -0800

    Finished day 3

--- 2019/day3_main.cpp
+++ 2019/day3_main.cpp
@@ -11,6 +11,7 @@ struct coord
 {
     int x;
     int y;
+    int step_count;
 
     bool operator==(const coord& other) const
     {
@@ -29,12 +30,14 @@ struct coord_hash
 void execute_instruction(coord& curr_pos, const std::string& instruction, std::unordered_set<coord, coord_hash>& uset)
 {
     int instr = std::stoi(instruction.substr(1));
+
     switch (instruction[0])
     {
         case 'U': 
         {
             for (int i = 0; i < instr; ++i)
             {
+                ++curr_pos.step_count;
                 curr_pos.y -= 1;
                 uset.insert(curr_pos);
             }
@@ -43,6 +46,7 @@ void execute_instruction(coord& curr_pos, const std::string& instruction, std::u
         {
             for (int i = 0; i < instr; ++i)
             {
+                ++curr_pos.step_count;
                 curr_pos.y += 1;
                 uset.insert(curr_pos);
             }
@@ -51,6 +55,7 @@ void execute_instruction(coord& curr_pos, const std::string& instruction, std::u
         {
             for (int i = 0; i < instr; ++i)
             {
+                ++curr_pos.step_count;
                 curr_pos.x -= 1;
                 uset.insert(curr_pos);
             }
@@ -59,6 +64,7 @@ void execute_instruction(coord& curr_pos, const std::string& instruction, std::u
         {
             for (int i = 0; i < instr; ++i)
             {
+                ++curr_pos.step_count;
                 curr_pos.x += 1;
                 uset.insert(curr_pos);
             }
@@ -73,11 +79,12 @@ void part_one(const std::vector<std::string>& lines)
 
     for (int i = 0; i < lines.size(); ++i)
     {
-        coord start_pos = { 0, 0 };
+        coord start_pos = { 0, 0, 0 };
         std::vector<std::string> instructions = jumi::split(lines[i], ',');
         for (int j = 0; j < instructions.size(); ++j)
         {
             const std::string& instr = instructions[j];
+
             i == 0 ? execute_instruction(start_pos, instr, line1_visited)
                    : execute_instruction(start_pos, instr, line2_visited);
         }
@@ -103,7 +110,49 @@ void part_one(const std::vector<std::string>& lines)
         }
     }
 
-    std::cout << "[Part One] The mininmum distance for an intersection is: " << min_distance << '\n';
+    std::cout << "[Part One] The minimum distance for an intersection is: " << min_distance << '\n';
+}
+
+void part_two(const std::vector<std::string>& lines)
+{
+    std::unordered_set<coord, coord_hash> line1_visited;
+    std::unordered_set<coord, coord_hash> line2_visited;
+
+    for (int i = 0; i < lines.size(); ++i)
+    {
+        coord start_pos = { 0, 0, 0 };
+        std::vector<std::string> instructions = jumi::split(lines[i], ',');
+        for (int j = 0; j < instructions.size(); ++j)
+        {
+            const std::string& instr = instructions[j];
+
+            i == 0 ? execute_instruction(start_pos, instr, line1_visited)
+                   : execute_instruction(start_pos, instr, line2_visited);
+        }
+    }
+
+    std::unordered_set<coord, coord_hash> intersection;
+    for (const coord& c : line1_visited)
+    {
+        if (line2_visited.find(c) != line2_visited.end())
+        {
+            coord new_coord = c;
+            new_coord.step_count += line2_visited.find(c)->step_count;
+            intersection.insert(new_coord);
+        }
+    }
+
+    int min_distance = INT_MAX;
+
+    for (const coord& c : intersection)
+    {
+        if (c.step_count < min_distance)
+        {
+            min_distance = c.step_count;
+        }
+    }
+
+    std::cout << "[Part Two] The minimum distance for an intersection is: " << min_distance << '\n';
 }
 
 int main()
@@ -111,4 +160,5 @@ int main()
     std::fstream file = jumi::open_file("input/day3_input.txt");
     std::vector<std::string> contents = jumi::read_lines(file);
     part_one(contents);
+    part_two(contents);
 }
